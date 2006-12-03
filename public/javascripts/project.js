dba3ff3f7399d87366a5b861a7c5810e5ae6b802
Phil Crosby <phil.crosby@gmail.com>
2006-12-03 06:53:28 +0000

    more refactorings

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -3,7 +3,7 @@ data=[]
 
 chartData=[12,22,10,26];
 chartData=[.5,2,5,5];
-
+chartData=[1,8,1];
 // Dean Edwards/Matthias Miller/John Resig
 
 function init() {
@@ -160,19 +160,15 @@ function populate(){
   tb=new TableDisplay("Most recent referers",["Recent referer", "Visited"],referersRecentData,2,referersWithDate);
   $("referers_recent").innerHTML=tb.buildTable();
 
-  
-  //$("referers_recent").innerHTML=table("Most recent referers",["Recent referer"],recentReferers,1,refererRecentInner);
-//   $("referers_unique").innerHTML=
-//     table("Newest unique referers",["Referer","First&nbsp;visited"],
-//     referersUnique,2,refererUniqueInner);  
-//   drawChart("chart",chartData);
+  pg = new PieGraphDisplay("chart",chartData);
+  pg.drawChart();
+  //drawChart("chart",chartData);
 }
 
 
 /*
  * Table display
  */
-
 TableDisplay=Class.create();
 
 TableDisplay.prototype={
@@ -206,9 +202,10 @@ TableDisplay.prototype={
     html+"</tr>"
     return html;
   },
-
-
 }
+
+
+
 DisplayHelper = Class.create();
 DisplayHelper.Methods={
   timeAgo: function(date){
@@ -245,6 +242,8 @@ DisplayHelper.Methods={
   }
 }
 Object.extend(DisplayHelper,DisplayHelper.Methods);
+
+
 
 function hitsWeek(i,data){
   //var c=classString(i, function(i){(i+now+1 > 24 ? " old" : "")});
@@ -289,252 +288,272 @@ function classString(i, func){
 
 
 
+/*
+ * Line graph drawing
+ */
 
-
-function drawGraph(id, data){
-  var p=$(id);
-  var g=document.createElement("div");
-  g.className="graph";
-  //size=120;
-  size=200;
-  //size=150;
-  var imgs=[]
-  var hwidth=size/(data.length-1);
- 
-  // relativize
-  var max=0;
-  data.each(function(e){ if (e>max) max=e; });
+LineGraph=Class.create();
+LineGraph.prototype={
+  initialize: function(id,data){
+    this.element=$(id);
+    this.data=GraphDisplay.relativize(data);
+  },
+  drawGraph: function(){  
+    // Graph container
+    var g=document.createElement("div");
+    g.className="graph";
+    //size=120;
+    size=200;
+    //size=150;
+    var imgs=[]
+    var hwidth=size/(data.length-1);   
   
-  for (i=0;i<data.length;i++){ 
-    data[i]=Math.floor((data[i]/max)*size); 
+    for (i=1;i<data.length; i++){
+      var div=document.createElement("div");
+      div.className="color";
+      div.id=i+"";
+      var img=document.createElement("img");    
+      
+      // Height of the point before this one
+      var prevHeight=data[i-1] ? data[i-1] : 0;
+      
+      // Whether the line is pointing up. Up=1, down=-1
+      var u=prevHeight<data[i] ? 1 : -1;
+      img.src= (u==1 ? "/images/line.gif" : "/images/lined.gif");    
+      img.className="line";
+      
+      img.style.width=hwidth+"px";
+      div.style.width=hwidth+"px";
+      
+      // difference in our heights
+      var h=data[i]-prevHeight;
+      //console.log(h);
+      
+      // amount of space there is above the previous element
+      t=size-prevHeight;
+      //console.log(t);
+      
+      img.style.height=h*u + "px";
+      
+      var ourTop = t-(u>0  ? h : 0)
+      //div.style.height=1+"px";
+      div.style.height=size-(h*u)-ourTop+"px";
+      
+      div.style.top=ourTop+(h*u)+"px";
+      img.style.top=ourTop+"px";
+      div.style.left=(i-1)*hwidth+ "px";
+      img.style.left=(i-1)*hwidth+ "px";
+      
+      g.appendChild(img);
+      g.appendChild(div);
+    } 
+    
+    this.element.appendChild(g);
   }
+}
+  
+  
+/*
+ * Pie chart graphing
+ */
 
-  for (i=1;i<data.length; i++){
-    var div=document.createElement("div");
-    div.className="color";
-    div.id=i+"";
+PieGraphDisplay = Class.create();
+PieGraphDisplay.prototype={
+  initialize: function(id,data){
+    this.element=$(id);
+    this.size=150;
+    this.qsize=this.size/2;
+    this.data=[];
+    // relativize data
+    for (var i=0;i<data.length;i++){ this.data[i]=Math.floor((data[i]/data.length)*360);}
+    //this.data=data;
+  },
+
+  drawChart: function (){   
+    var placeholder=document.createElement("div")
+    placeholder.style.position="absolute";
+    placeholder.style.width=px(this.size);
+    placeholder.style.height=px(this.size);
+    //placeholder.className="placeholder";
+  
+    /*var max=0;
+    var total=0;
+    data.each(function(e){ total+=e; if (e>max) max=e; });  */ 
+    
+    //console.log(data);
+    
+    for (var i=0;i<this.data.length-1;i++){
+      this.graphQuadrant(i,this.data,placeholder);
+    }
+    placeholder.style.backgroundColor=page.colors[this.data.length-1];
+    
+  //   graphQuadrant(0,data, placeholder);
+  //   graphQuadrant(1,data, placeholder);
+  //   graphQuadrant(2,data, placeholder);
+  //   graphQuadrant(3,data, placeholder);
+    
+    this.element.appendChild(placeholder);
+  },
+
+  graphQuadrant: function(i,data, placeholder){
+    var v=data[i]+this.sumPrevious(i,data);
+    //console.log("data:" + v);
+    console.log("graphing " + i + " a" + v);
+    // quadrant
+    //console.log(v);
+    //var q=cap(Math.floor(v/90),3);
+    var q=Math.floor(v/90);
+    
+    // Multiplier
+    var m=90*(q+1);
+  
+    // Find out how many degrees we are into this quadrant, ie.
+    // 300 is 30 degrees into quad three
+    var deg = (m-v)/90;    
+    
+    var w,h;
+    qs=this.size;s=this.size;
+    if (q==0){
+      w=(v <=(45+q*90) ? qs : s*(deg));
+      h=(v >=(45+q*90) ? qs : s*(1-deg));
+    }else if (q==1){
+      h=(v <=(45+q*90) ? qs : s*(deg));
+      w=(v >=(45+q*90) ? qs : s*(1-deg));
+    }else if (q==2){
+      //a=(v <=(45+q*90) ? qsize : size*(1-v/m));
+      w=(v <=(45+q*90) ? qs : s*(deg));
+      h=(v >=(45+q*90) ? qs : s*(1-deg));  
+    }else{
+      h=(v <=(45+q*90) ? qs : s*(deg));
+      //a=(v >=(45+q*90) ? qsize : size*(1-v/m));
+      w=(v >=(45+q*90) ? qs : s*(1-deg));
+    }
+  
+  
+  
     var img=document.createElement("img");    
+    img.className="chart_image";  
+    img.style.width=w+"px";
+    img.style.height=h+"px";
+    img.style.zIndex="1";
+    o1=(q==1||q==2) ? 0 : 1;
+    o2=(q==2||q==3) ? 0 : 1;
     
-    // Height of the point before this one
-    var prevHeight=data[i-1] ? data[i-1] : 0;
+    img.style.left=this.qsize-o1*w+"px";
     
-    // Whether the line is pointing up. Up=1, down=-1
-    var u=prevHeight<data[i] ? 1 : -1;
-    img.src= (u==1 ? "/images/line.gif" : "/images/lined.gif");    
-    img.className="line";
-    
-    img.style.width=hwidth+"px";
-    div.style.width=hwidth+"px";
-    
-    // difference in our heights
-    var h=data[i]-prevHeight;
-    //console.log(h);
-    
-    // amount of space there is above the previous element
-    t=size-prevHeight;
-    //console.log(t);
-    
-    img.style.height=h*u + "px";
-    
-    var ourTop = t-(u>0  ? h : 0)
-    //div.style.height=1+"px";
-    div.style.height=size-(h*u)-ourTop+"px";
-    
-    div.style.top=ourTop+(h*u)+"px";
-    img.style.top=ourTop+"px";
-    div.style.left=(i-1)*hwidth+ "px";
-    img.style.left=(i-1)*hwidth+ "px";
-    
-    g.appendChild(img);
-    g.appendChild(div);
-  } 
+    img.style.top=this.qsize-o2*h + "px";
   
-  p.appendChild(g);
+    img.src=this.imageForQuadrant(i,q);
+    
+    img.style.zIndex=data.length*2-i*2+"";
+    //console.log(data.length*2-i*2+"");
+    
+    this.drawFillerBoxes(placeholder,page.colors[i],q,this.data.length*2-i*2-1,w,h);
+    
+    placeholder.appendChild(img);
+  },
+
+  drawFillerBoxes: function(element,color,q,level,w,h){  
+    for (i=0; i<q; i++)
+    {
+      element.appendChild(this.drawFillerBox(color, i, level,0,0));
+    }
+    // Make sure we should add the last element..
+    
+    if ( ((q==0 || q==2) && h<qsize) ||
+      ((q==1 || q==3) && w<qsize) )
+      return;
+    
+  //   console.log("appendling child." + q);
+  //   console.log(h + qsize + "");
+    d= this.drawFillerBox(color,q,level,w,h);
+    if (d)
+      element.appendChild(d);
+    //element.appendChild(drawFillerBox(color,q,level,w,h));
+  },
+  // Puts a square in the given quadrant, next to the angle image that has a width & height of w & h
+  drawFillerBox: function(color, q, level,w,h){  
+    //return null;
+    var div=document.createElement("div");
+    div.style.backgroundColor=color;
+    div.className="chart_panel";
+    
+  //   t = (q==2 || q==3) ? size/2 : 0;
+  //   div.style.top=t + "px";
+  //   l = (q==1 || q==2) ? size/2 : 0;
+  //     //have to special case this one
+  //   if (q==0){
+  //      div.style.width=(qsize-w)+"px";
+  //      //div.style.height=(qsize-h)+"px";     
+  //   }
+  //   div.style.left=l + "px";
+    div.style.zIndex=level+"";
+    
+    // div dimentions
+    var dw,dh,dt,dl;
+    qs=this.qsize;
+    
+    dw = ((q==1 || q==3) ? w : qs-w );
+    
+    
+    //console.log(px( ((q==1 || q==3) ? w : qsize-w )));
+    if (q==0){
+  //      div.style.width=px(qsize-w);
+      dl=px(0); 
+    }
+    if (q==1){
+  //     div.style.width=px(w);
+      dl=qs;
+      dh=qs-h;
+    }
+    if (q==2){
+  //     div.style.width=px(qsize-w);
+      dl=qs+w;
+      dt=qs;  
+      dh=qs-h;
+    }
+    if (q==3){
+  //     div.style.width=px(w);
+      dh=qs-h;
+      dt=qs+h;
+      //div.style.left=px(0);
+    }
+    
+    div.style.left=px(dl);
+    div.style.top=px(dt);
+    if (dw==0) dw=qs;
+    div.style.width=px(dw);
+    div.style.height=px(dh);
+  
+    return div;
+  },
+  imageForQuadrant: function (i,q){
+    return "/images/c/line" + i + "" + q + ".png"
+  },
+  // Returns the sum of all entries up to i in an array
+  sumPrevious: function(i,array){
+    return array.inject(0, function (a,v){return a+v;}) - array[i];
+  }
 }
 
-
-function previousValues(i,arr){
-  var sum=0;
-  for (var j=0;j<i;j++){
-    sum+=arr[j];
-  }
-  return sum;
-}
-
-
-function drawChart(id, data){
-  size=150;
-  qsize=size/2;
-  
-  var placeholder=document.createElement("div")
-  placeholder.style.position="absolute";
-  placeholder.style.width=px(size);
-  placeholder.style.height=px(size);
-  //placeholder.className="placeholder";
-
-  var max=0;
-  var total=0;
-  data.each(function(e){ total+=e; if (e>max) max=e; });  
-  for (var i=0;i<data.length;i++){ data[i]=Math.floor((data[i]/total)*360);}
-  
-  //console.log(data);
-  
-  for (var i=0;i<data.length-1;i++){
-    graphQuadrant(i,data,placeholder);
-  }
-  placeholder.style.backgroundColor=colors[data.length-1];
-  
-//   graphQuadrant(0,data, placeholder);
-//   graphQuadrant(1,data, placeholder);
-//   graphQuadrant(2,data, placeholder);
-//   graphQuadrant(3,data, placeholder);
-  
-  $(id).appendChild(placeholder);
-}
-
-function graphQuadrant(i,data, placeholder){
-  v=data[i]+previousValues(i,data);
-  //console.log("data:" + v);
-  console.log("graphing " + i + " a" + v);
-  // quadrant
-  //console.log(v);
-  //var q=cap(Math.floor(v/90),3);
-  var q=Math.floor(v/90);
-  
-  // Multiplier
-  var m=90*(q+1);
-
-  // Find out how many degrees we are into this quadrant, ie.
-  // 300 is 30 degrees into quad one
-  var deg = (m-v)/90;
-  
-  
-  var w,h;
-  if (q==0){
-    w=(v <=(45+q*90) ? qsize : size*(deg));
-    h=(v >=(45+q*90) ? qsize : size*(1-deg));
-  }else if (q==1){
-    h=(v <=(45+q*90) ? qsize : size*(deg));
-    w=(v >=(45+q*90) ? qsize : size*(1-deg));
-  }else if (q==2){
-    //a=(v <=(45+q*90) ? qsize : size*(1-v/m));
-    w=(v <=(45+q*90) ? qsize : size*(deg));
-    h=(v >=(45+q*90) ? qsize : size*(1-deg));  
-  }else{
-    h=(v <=(45+q*90) ? qsize : size*(deg));
-    //a=(v >=(45+q*90) ? qsize : size*(1-v/m));
-    w=(v >=(45+q*90) ? qsize : size*(1-deg));
-  }
-
-
-
-  img=document.createElement("img");    
-  img.className="chart_image";  
-  img.style.width=w+"px";
-  img.style.height=h+"px";
-  img.style.zIndex="1";
-  o1=(q==1||q==2) ? 0 : 1;
-  o2=(q==2||q==3) ? 0 : 1;
-  
-  img.style.left=qsize-o1*w+"px";
-  
-  img.style.top=qsize-o2*h + "px";
-
-  img.src=imageForQuadrant(i,q);
-  
-  img.style.zIndex=data.length*2-i*2+"";
-  //console.log(data.length*2-i*2+"");
-  
-  drawFillerBoxes(placeholder,colors[i],q,data.length*2-i*2-1,w,h);
-  
-  placeholder.appendChild(img);
-}
-
-function drawFillerBoxes(element,color,q,level,w,h){
-
-  for (i=0; i<q; i++)
-  {
-    element.appendChild(drawFillerBox(color, i, level,0,0));
-  }
-  // Make sure we should add the last element..
-  
-  if ( ((q==0 || q==2) && h<qsize) ||
-    ((q==1 || q==3) && w<qsize) )
-    return;
-  
-//   console.log("appendling child." + q);
-//   console.log(h + qsize + "");
-  d= drawFillerBox(color,q,level,w,h);
-  if (d)
-    element.appendChild(d);
-  //element.appendChild(drawFillerBox(color,q,level,w,h));
-}
-// Puts a square in the given quadrant, next to the angle image that has a width & height of w & h
-function drawFillerBox(color, q, level,w,h){  
-  //return null;
-  div=document.createElement("div");
-  div.style.backgroundColor=color;
-  div.className="chart_panel";
-   
-//   t = (q==2 || q==3) ? size/2 : 0;
-//   div.style.top=t + "px";
-//   l = (q==1 || q==2) ? size/2 : 0;
-//     //have to special case this one
-//   if (q==0){
-//      div.style.width=(qsize-w)+"px";
-//      //div.style.height=(qsize-h)+"px";     
-//   }
-//   div.style.left=l + "px";
-  div.style.zIndex=level+"";
-  
-  // div dimentions
-  var dw,dh,dt,dl;
-  
-  dw = ((q==1 || q==3) ? w : qsize-w );
-  
-  
-  //console.log(px( ((q==1 || q==3) ? w : qsize-w )));
-  if (q==0){
-//      div.style.width=px(qsize-w);
-     dl=px(0); 
-  }
-  if (q==1){
-//     div.style.width=px(w);
-    dl=qsize;
-    dh=qsize-h;
-  }
-  if (q==2){
-//     div.style.width=px(qsize-w);
-    dl=qsize+w;
-    dt=qsize;  
-    dh=qsize-h;
-  }
-  if (q==3){
-//     div.style.width=px(w);
-    dh=qsize-h;
-    dt=qsize+h;
-    //div.style.left=px(0);
-  }
-  
-  div.style.left=px(dl);
-  div.style.top=px(dt);
-  if (dw==0) dw=qsize;
-  div.style.width=px(dw);
-  div.style.height=px(dh);
-
-   return div;
-
-}
 function px(v) {  return v + "px";}
 
-function imageForQuadrant(i,q){
-  base="/images/c/line";
-  
-  return base +  i +"" +q+".png"
+
+LineGraph.Methods={
+// relativize
+  relativize:function(data){
+    var max=0;
+    data.each(function(e){ if (e>max) max=e; });
+    for (i=0;i<data.length;i++){ 
+      data[i]=Math.floor((data[i]/max)*size); 
+    }
+  }
 }
+Object.extend(LineGraph,LineGraph.Methods);
+  
 
 
 
 
 
-chartData=[1,8,1];
+
