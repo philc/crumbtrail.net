2db3ce281cd8de2516bdd022d757ab4a8871d9d9
Mike Quinn <mikejquinn@gmail.com>
2006-12-04 06:54:21 +0000

    fixed total referral bug

--- app/models/total_referral.rb
+++ app/models/total_referral.rb
@@ -2,26 +2,29 @@ class TotalReferral < ActiveRecord::Base
   belongs_to :project
   belongs_to :referer
 
-  def self.increment_referer(project_id, time, referer)
-    total_referral = find_by_project_id_and_referer_id(project_id, referer.id)
+  def self.increment_referer(request)
+    project = request.project
+    referer = request.referer
+
+    total_referral = find_by_project_id_and_referer_id(project.id, referer.id)
 
     if total_referral.nil?
-      total_referral = new(:project_id => project_id,
+      total_referral = new(:project_id => project.id,
                            :referer_id => referer.id,
                            :count => 0,
-                           :first_visit => time,
-                           :recent_visit => time)
+                           :first_visit => request.time,
+                           :recent_visit => request.time)
     else
-      total_referral.recent_visit = time
+      total_referral.recent_visit = request.time
     end
 
     total_referral.count += 1
     total_referral.save
   end
   
-  def self.get_recent_unique(project_id)
+  def self.get_recent_unique(project)
     return find(:all,
-                :conditions => ["project_id = ?", project_id],
+                :conditions => ["project_id = ?", project.id],
                 :order      => "first_visit DESC",
                 :limit      => 10)
   end
