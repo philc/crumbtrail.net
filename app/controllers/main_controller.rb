2bd89c3f4f2cca10c0c9096b22f505885047b617
Phil Crosby <phil.crosby@gmail.com>
2006-12-06 15:42:57 +0000

    updated super awesome js template code

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 class MainController < ApplicationController
 
   def index
+    puts params
+    puts cookies
   end
 end
--- public/javascripts/code_template.js
+++ public/javascripts/code_template.js
@@ -1,23 +1,25 @@
 <script type="text/javascript">
 function tracker(){
 
+// alert("tracking");
 //var req = "http://123.super.com/demo/test";
-var req = "http://192.168.0.30/inkforword.png";
+p=1050;var req = "http://localhost/philisoft/personal/images/inkforword.png";
 e=document.createElement("script");
 
 r=""+document.referrer;r=(r.length<=0)?"":'r='+escape(r)+'&';
-e.setAttribute("src", req + '?'+r+'p=1050&c=' + (new Date()).getTime()%9000);
+e.setAttribute("src", req + '?'+r+'p='+p+'&u='+(document.cookie.indexOf(p)>=0?0:1)+'&c=' +
+ (new Date()).getTime()%9000);
 document.getElementsByTagName("head").item(0).appendChild(e);
 
 // Create the cookie after the first request, so the first hit is logged as new
-host="192.168.0.30"
+host="http://localhost/"
+
 // 1 hour later.
 d=new Date(); d.setTime(d.getTime()+3600000);
 d.toGMTString();
 
-document.cookie='1050=1; expires=' + d.toGMTString() + '; domain='+host;
-
+document.cookie='1050=1; expires=' + d.toGMTString() + ';// domain='+host;
 }
 if (typeof window.onload!='function'){window.onload=tracker;}
-else{ f1=window.onload;	window.onload=function(){f1();tracker();}}
+else{ f1=window.onload; window.onload=function(){f1();tracker();}}
 </script>
