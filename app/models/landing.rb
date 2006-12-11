fb478956cfd99d98e5ee62ce99f7c2e48c5425be
Mike Quinn <mikejquinn@gmail.com>
2006-12-11 17:27:34 +0000

    moving landing.rb

---
+++ app/models/landing.rb
@@ -0,0 +1,21 @@
+class LandingUrl < ActiveRecord::Base
+  has_many   :recent_referrals
+  has_many   :total_referrals
+  belongs_to :referers
+  belongs_to :recent_referrals
+  has_man    :search_referrals
+
+  def self.get_most_popular(project, limit)
+    return find(:all, 
+                :conditions => ['project_id = ?', project.id],
+                :limit => limit,
+                :order => "count DESC")
+  end
+
+  def self.get_most_recent(project, limit)
+    return find(:all,
+                :conditions => ['project_id = ?', project.id],
+                :limit => limit,
+                :order => "last_visit DESC")
+  end
+end
