a6c33254af47f965150817532173946fc6f94dcb
Phil Crosby <phil.crosby@gmail.com>
2007-01-10 01:29:23 +0000

    working on some css

--- db/migrate/007_load_timezone_data.rb
+++ db/migrate/007_load_timezone_data.rb
@@ -1,5 +1,6 @@
 class LoadTimezoneData < ActiveRecord::Migration
   def self.up
+    
     MainHelper::SUPPORTED_TIMEZONES.each do |t|
       Zone.create(:identifier=>t,
                  :offset=>TZInfo::Timezone.get(t).current_period.utc_offset/60.0/60)  
