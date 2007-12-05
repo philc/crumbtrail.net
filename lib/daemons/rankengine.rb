680546f66d8f7b2377f8e18311cf1e5f1d161e13
Mike Quinn <mikejquinn@gmail.com>
2007-12-05 04:52:53 +0000

    Added daemon plugin for managing logreader and rankengine processes. Added a daemon for the rank engine

---
+++ lib/daemons/rankengine.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+#You might want to change this
+ENV["RAILS_ENV"] ||= "development"
+
+require File.dirname(__FILE__) + "/../../config/environment"
+require File.dirname(__FILE__) + "/../../searchparser/ranklib.rb"
+
+@rankdir = File.dirname(__FILE__) + "/../../tmp/rankengine/"
+@termsfilename = @rankdir + "querylist"
+@resultsfilename = @rankdir + "resultslist"
+
+$running = true;
+Signal.trap("TERM") do 
+  $running = false
+end
+
+while($running) do
+  
+  ActiveRecord::Base.logger << "This daemon is still running at #{Time.now}.\n"
+  ActiveRecord::Base.logger << "fileame #{@termsfilename}"
+  
+  if(! FileTest.exists? @rankdir)
+    FileUtils.mkdir(@rankdir, :mode => 0755)
+  end
+  
+  termsFileName = "@termsfilename.#{Date.today.to_s}"
+  resultsFileName = "@resultsfilename.#{Date.today.to_s}"
+  
+  # Read the list of queryies we need to make from
+  # our database, and store them in a file
+  file = File.open(termsFileName, 'w')
+  write_terms_file(file)
+  file.close()
+
+  # Now, read that file and perform the queries.
+  # Store the results in another file.
+  termsfile = File.open(termsFileName, 'r')
+  resultsfile = File.open(resultsFileName, 'w')
+  get_rankings(termsfile, resultsfile, false)
+  termsfile.close()
+  resultsfile.close()
+
+  # Read the result file and store the ranks back in
+  # our database.
+  resultsfile = File.open(resultsFileName, 'r')
+  store_rank_results(resultsfile)
+  resultsfile.close()
+    
+  sleep 60
+end
